d1a43b4c3c1cc9e3e53f22fa66eeef839ec46363
tannt-tv <tannt19092001@gmail.com>
2025-02-26 23:53:57 +0700

    config repository

--- APIs/CusCake.Infrastructures/UnitOfWork.cs
+++ APIs/CusCake.Infrastructures/UnitOfWork.cs
@@ -12,12 +12,49 @@ namespace CusCake.Infrastructures
             AppDbContext appDbContext,
             ICustomerRepository customerRepository,
             IBakeryRepository bakeryRepository,
-            IStorageRepository storageRepository)
+            IStorageRepository storageRepository,
+            IAvailableCakeRepository availableCakeRepository,
+            IBankEventRepository bankEventRepository,
+            ICakeDecorationDetailRepository cakeDecorationDetailRepository,
+            ICakeDecorationRepository cakeDecorationRepository,
+            ICakeExtraDetailRepository cakeExtraDetailRepository,
+            ICakeExtraRepository cakeExtraRepository,
+            ICakeMessageRepository cakeMessageRepository,
+            ICakePartDetailRepository cakePartDetailRepository,
+            ICakePartRepository cakePartRepository,
+            ICakeReviewRepository cakeReviewRepository,
+            ICustomCakeRepository customCakeRepository,
+            ICustomerVoucherRepository customerVoucherRepository,
+            INotificationRepository notificationRepository,
+            IOrderDetailRepository orderDetailRepository,
+            IOrderRepository orderRepository,
+            IOrderSupportRepository orderSupportRepository,
+            ITransactionRepository transactionRepository,
+            IVoucherRepository voucherRepository
+        )
         {
             _appDbContext = appDbContext;
             CustomerRepository = customerRepository;
             BakeryRepository = bakeryRepository;
             StorageRepository = storageRepository;
+            AvailableCakeRepository = availableCakeRepository;
+            BankEventRepository = bankEventRepository;
+            CakeDecorationDetailRepository = cakeDecorationDetailRepository;
+            CakeDecorationRepository = cakeDecorationRepository;
+            CakeExtraDetailRepository = cakeExtraDetailRepository;
+            CakeExtraRepository = cakeExtraRepository;
+            CakeMessageRepository = cakeMessageRepository;
+            CakePartDetailRepository = cakePartDetailRepository;
+            CakePartRepository = cakePartRepository;
+            CakeReviewRepository = cakeReviewRepository;
+            CustomCakeRepository = customCakeRepository;
+            CustomerVoucherRepository = customerVoucherRepository;
+            NotificationRepository = notificationRepository;
+            OrderDetailRepository = orderDetailRepository;
+            OrderRepository = orderRepository;
+            OrderSupportRepository = orderSupportRepository;
+            TransactionRepository = transactionRepository;
+            VoucherRepository = voucherRepository;
         }
 
         public ICustomerRepository CustomerRepository { get; }
@@ -25,6 +62,41 @@ namespace CusCake.Infrastructures
         public IBakeryRepository BakeryRepository { get; }
 
         public IStorageRepository StorageRepository { get; }
+
+        public IAvailableCakeRepository AvailableCakeRepository { get; }
+
+        public IBankEventRepository BankEventRepository { get; }
+
+        public ICakeDecorationDetailRepository CakeDecorationDetailRepository { get; }
+
+        public ICakeDecorationRepository CakeDecorationRepository { get; }
+
+        public ICakeExtraDetailRepository CakeExtraDetailRepository { get; }
+
+        public ICakeExtraRepository CakeExtraRepository { get; }
+
+        public ICakeMessageRepository CakeMessageRepository { get; }
+
+        public ICakePartDetailRepository CakePartDetailRepository { get; }
+
+        public ICakePartRepository CakePartRepository { get; }
+
+        public ICakeReviewRepository CakeReviewRepository { get; }
+
+        public ICustomCakeRepository CustomCakeRepository { get; }
+
+        public ICustomerVoucherRepository CustomerVoucherRepository { get; }
+
+        public INotificationRepository NotificationRepository { get; }
+        public IOrderDetailRepository OrderDetailRepository { get; }
+
+        public IOrderRepository OrderRepository { get; }
+
+        public IOrderSupportRepository OrderSupportRepository { get; }
+
+        public ITransactionRepository TransactionRepository { get; }
+
+        public IVoucherRepository VoucherRepository { get; }
 
         public async Task<bool> SaveChangesAsync() => (await _appDbContext.SaveChangesAsync()) > 0;
     }
